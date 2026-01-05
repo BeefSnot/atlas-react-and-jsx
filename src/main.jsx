7b01b9499200a46e95c46e70c6a6811439bb60de
James <james@jameshamby.me>
2026-01-05 11:45:07 -0600

    rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// Render the App component to the root element
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
